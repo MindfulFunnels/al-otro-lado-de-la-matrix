36f0155516973000af6f920be60482eb525eb48a
Lucas-Andres-GF <lucas.gallardo196304@alumnos.info.unlp.edu.ar>
2025-04-17 11:15:37 -0300

    Mejoras en la configuración y estilos de componentes, incluyendo ajustes de opacidad, alineación de texto y actualización de clases CSS. Se agregó una sección de evento en vivo y se optimizó la estructura de la página principal.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+  },
+
+  output: "server",
+
+  server: {
+    host: true, // Habilita el acceso desde la red local
   },
 
   integrations: [react()],
-  adapter: vercel()
+  adapter: vercel(),
 });
